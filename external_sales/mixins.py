36b2ee0ec299ce762f34cd44021602ffe01d8e46
zakee <zakee.tahawi@elkhawaga.com>
2026-03-13 15:35:44 +0200

    إضافة is_external_sales_director في mixins المبيعات الخارجية - إصلاح 403

--- external_sales/mixins.py
+++ external_sales/mixins.py
@@ -3,12 +3,13 @@ from django.core.exceptions import PermissionDenied
 
 
 class DecoratorDeptRequiredMixin(LoginRequiredMixin):
-    """Allow only decorator dept staff, managers, and superusers"""
+    """Allow only decorator dept staff, managers, directors, and superusers"""
 
     def dispatch(self, request, *args, **kwargs):
         user = request.user
         if not (
             user.is_superuser
+            or user.is_external_sales_director
             or user.is_decorator_dept_manager
             or user.is_decorator_dept_staff
             or user.is_sales_manager
@@ -18,12 +19,13 @@ class DecoratorDeptRequiredMixin(LoginRequiredMixin):
 
 
 class DecoratorManagerRequiredMixin(LoginRequiredMixin):
-    """Allow only decorator dept managers and superusers"""
+    """Allow only decorator dept managers, directors, and superusers"""
 
     def dispatch(self, request, *args, **kwargs):
         user = request.user
         if not (
             user.is_superuser
+            or user.is_external_sales_director
             or user.is_decorator_dept_manager
             or user.is_sales_manager
         ):
